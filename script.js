1ea08e6b236e5610a61d4f09278bbf750be839b9
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-06 00:41:57 +0530

    add operate function

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const add = (a, b) => a + b;
-const sub = (a, b) => a - b;
-const div = (a, b) => a / b;
-const mul = (a, b) => a * b;
+const subtract = (a, b) => a - b;
+const divison = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+
+const operate = (a, op, b) => {
+    switch (op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '/':
+            return divison(a, b);
+        case '*':
+            return multiply(a, b);
+        default:
+            break;
+    }
+};
