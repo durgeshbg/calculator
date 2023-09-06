eee4a2f0fcae7b3546a7a4754eaa40fee33e103e
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-06 17:05:10 +0530

    feat: add function to update display

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
+const display = document.querySelector('.display');
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const divison = (a, b) => a / b;
 const multiply = (a, b) => a * b;
 
 const operate = (a, op, b) => {
-    switch (op) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case '/':
-            return divison(a, b);
-        case '*':
-            return multiply(a, b);
-        default:
-            break;
-    }
+	switch (op) {
+		case '+':
+			return add(a, b);
+		case '-':
+			return subtract(a, b);
+		case '/':
+			return divison(a, b);
+		case '*':
+			return multiply(a, b);
+		default:
+			break;
+	}
 };
+
+const updateText = (text) => {
+	display.textContent += text;
+};
+
+document.querySelectorAll('.digit').forEach((button) =>
+	button.addEventListener('click', (e) => {
+		updateText(e.target.textContent);
+	})
+);
+
+document.querySelectorAll('.operator').forEach((button) =>
+	button.addEventListener('click', (e) => {
+		updateText(` ${e.target.textContent} `);
+	})
+);
